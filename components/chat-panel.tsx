0584297ab8c7a4af147b887bc71de23978680c87
yuliiastrizhalko <321gadzila12366@gmail.com>
2024-05-14 13:29:38 +0300

    prompt-boxes-padding-and-layout-changed

--- components/chat-panel.tsx
+++ components/chat-panel.tsx
@@ -99,12 +99,12 @@ const randomQuestions = randomNumbers()
       />
 
       <div className="mx-auto sm:max-w-2xl sm:px-4">
-        <div className="mb-4 grid grid-cols-2 gap-2 px-4 sm:px-0">
+        <div className="mb-4 grid gap-2 px-4 sm:px-0">
           {messages.length === 0 &&
             randomQuestions.map((example, index) => (
               <div
                 key={example.key}
-                className={`cursor-pointer rounded-lg border bg-white p-4 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
+                className={`cursor-pointer rounded-lg border bg-white p-2 hover:bg-zinc-50 dark:bg-zinc-950 dark:hover:bg-zinc-900 ${
                   index > 1 && 'hidden md:block'
                 }`}
                 onClick={async () => {
