3683d4d3b50cb632e514f5610727afabfc8fb211
mrw46 <mrw46@outlook.com>
2024-05-16 14:48:39 +0100

    adds alternative fs syntax

--- lib/mdReader.ts
+++ lib/mdReader.ts
@@ -1,9 +1,8 @@
-// import fs from 'fs/promises';
-import { promises as fs } from 'fs';
-import path from 'path';
-
-export async function readMarkdownFile(filePath: string): Promise<string> {
-  const fullPath = path.join(process.cwd(), filePath);
-  const fileContent = await fs.readFile(fullPath, 'utf8');
-  return fileContent;
+import * as fs from 'fs';
+import * as path from 'path';
+export function readMarkdownFile(filePath: string): string {
+    const fullPath = path.join(process.cwd(), filePath);
+    const data = fs.readFileSync(fullPath, { encoding: 'utf8', flag: 'r' });
+    return data   
 }
+
