c1607953501e42dfde9daad29359d43c971970e9
Nada Shetty <nada.shetty@yahoo.com>
2024-05-16 14:30:38 +0100

    update dependencies

--- lib/mdReader.ts
+++ lib/mdReader.ts
@@ -1,4 +1,5 @@
-import fs from 'fs/promises';
+// import fs from 'fs/promises';
+import { promises as fs } from 'fs';
 import path from 'path';
 
 export async function readMarkdownFile(filePath: string): Promise<string> {
