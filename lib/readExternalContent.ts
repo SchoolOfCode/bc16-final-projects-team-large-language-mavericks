edb70728b8b8e58314dc22666eeadeb631af388d
mrw46 <mrw46@outlook.com>
2024-05-24 14:45:04 +0100

    type guard function added to fix typing errors in notion api call

--- lib/readExternalContent.ts
+++ lib/readExternalContent.ts
@@ -1,27 +1,47 @@
 import { Client } from '@notionhq/client';
-import {NotionToMarkdown} from 'notion-to-md'
+import { NotionToMarkdown } from 'notion-to-md';
+import { PageObjectResponse } from "@notionhq/client/build/src/api-endpoints";
 
 const notion = new Client({ auth: process.env.NOTION_API_KEY });
 const n2m = new NotionToMarkdown({ 
   notionClient: notion
  });
 
+function isPageObjectResponse(response: any): response is PageObjectResponse {
+  return response.object === 'page' && 'properties' in response;
+}
+
  // function to retrieve url, pageName and pageId, filtered by Tag name
-export async function getUrl (){
+export async function getUrl() {
   const databaseId = 'e0279db6b37943308048d1a36b6aa66d';
   const response = await notion.databases.query({
     database_id: databaseId,
   });
+
   console.log(response);
-  let returnObject = [];
+  let returnObject: Array<{ url: string, pageId: string, pageName: string, tag: string[] }> = [];
+
   for (let i = 0; i < response.results.length; i++) {
-    returnObject.push({"url": response.results[i]["properties"]["URL"]['url'], 
-    "pageId": response.results[i]["id"], 
-    "pageName": response.results[i]["properties"]["Name"]["title"][0]["plain_text"],
-    "tag": response.results[i]["properties"]["Tags"]["multi_select"].map((tag) => tag["name"])
-  })
+    const page = response.results[i];
+    if (isPageObjectResponse(page)) {
+      const properties = page.properties;
+      
+      const url = properties["URL"]?.type === 'url' ? properties["URL"].url : null;
+      const name = properties["Name"]?.type === 'title' && properties["Name"].title.length > 0 ? properties["Name"].title[0].plain_text : null;
+      const tags = properties["Tags"]?.type === 'multi_select' ? properties["Tags"].multi_select.map(tag => tag.name) : [];
+
+      if (url && name) {
+        returnObject.push({
+          "url": url,
+          "pageId": page.id,
+          "pageName": name,
+          "tag": tags
+        });
+      }
+    }
   }
-  return returnObject
+
+  return returnObject;
 }
 
 // read Notion page content and return in md format
